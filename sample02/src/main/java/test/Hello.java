37ebd8813639e3c6da3cfa971f972d146ac80daf
Songsungeun <korea.sesong@gmail.com>
2016-09-20 10:45:26 +0900

    .

--- sample02/src/main/java/test/Hello.java
+++ sample02/src/main/java/test/Hello.java
@@ -5,6 +5,16 @@ public class Hello {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
 
+    int i = 1;
+    int j = 0;
+    int s = -1;
+    for (int k = 0; k < 100; k++) {
+      if ((k % 2) == 1) {
+        s = s * (-1);
+      }
+      i = i + (j * s);
+      j += 1;
+    }
   }
 
 }
