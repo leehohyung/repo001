78e12ac890e16d4115aeb8ee05ea3d4ce4d32bf5
Kevinsmacbookpro <Kevinsmacbookpro@192.168.0.211>
2016-09-20 10:44:37 +0900

    .

--- sample02/src/main/java/test/Hello.java
+++ sample02/src/main/java/test/Hello.java
@@ -4,6 +4,11 @@ public class Hello {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
+    
+    int a = 20;
+    int b = 50;
+    
+    System.out.println(a + b);
 
   }
 
